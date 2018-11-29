796715cfa44a10128e584a01d35c42c5cfe5ef12
Marcus Green <marcusavgreen@gmail.com>
2018-11-29 08:27:08 +0000

    Update mobile.php

--- db/mobile.php
+++ db/mobile.php
@@ -13,7 +13,7 @@ $addons = array(
                     'mobile_get_YOURQTYPENAME' => array(),
                 ), // Function needs caching for offline.
                'styles' => array(
-                    'url' => '/question/type/YOURQTYPENAME/styles_app.css',
+                    'url' => '/question/type/YOURQTYPENAME/mobile/styles_app.css',
                     'version' => '1.00'
                 ),
                 'lang' => array(
