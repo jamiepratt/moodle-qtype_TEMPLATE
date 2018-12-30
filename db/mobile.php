909361400ca16e233690cbff3c57ce9874c1fe5c
Marcus Green <marcusavgreen@gmail.com>
2018-12-30 21:55:59 +0000

    Update mobile.php

--- db/mobile.php
+++ db/mobile.php
@@ -1,25 +1,51 @@
-$addons = array(
-    "qtype_YOURQTYPENAME" => array(
-        "handlers" => array( // Different places where the add-on will display content.
-            'YOURQTYPENAME' => array( // Handler unique name (can be anything).
-                'displaydata' => array(
-                    'title' => 'Qtype Name',
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * YOURQTYPENAME question type  capability definition
+ *
+ * @package    qtype_YOURQTYPENAME
+ * @copyright  20XX Author Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+$addons = [
+    "qtype_YOURQTYPENAME" => [
+        "handlers" => [ // Different places where the add-on will display content.
+            'YOURQTYPENAME' => [ // Handler unique name (can be anything).
+                'displaydata' => [
+                    'title' => 'YOURQTYPENAME question',
                     'icon' => '/question/type/YOURQTYPENAME/pix/icon.gif',
-                    'class' => '', //What does this do?
-                ),
+                    'class' => '',
+                ],
                 'delegate' => 'CoreQuestionDelegate', // Delegate (where to display the link to the add-on).
                 'method' => 'mobile_get_YOURQTYPENAME',
-                'offlinefunctions' => array(
-                    'mobile_get_YOURQTYPENAME' => array(),
-                ), // Function needs caching for offline.
-               'styles' => array(
+                'offlinefunctions' => [
+                    'mobile_get_YOURQTYPENAME' => [],
+                ], // Function needs caching for offline.
+                'styles' => [
                     'url' => '/question/type/YOURQTYPENAME/mobile/styles_app.css',
                     'version' => '1.00'
-                ),
-                'lang' => array(
-                    array('YOURQTYPENAME name', 'pluginname')
-                )
-            )
-        ),
-    )
-);
+                ]
+            ]
+        ],
+        'lang' => [
+                    ['pluginname', 'qtype_YOURQTYPENAME'],
+        ],
+    ]
+];
