a2070ba14ef35618971d9c26575ed1bee07dd550
Marcus Green <marcusavgreen@gmail.com>
2020-10-25 17:28:27 +0000

    added ES6 example in the amd folder

---
+++ amd/src/example.js
@@ -0,0 +1,32 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * YOURQTYPENAME setup the ES6 javascript for the question type
+ * https://docs.moodle.org/dev/Javascript_Modules#ES6_Modules_.28Moodle_v3.8_and_above.29
+ *
+ * @package    qtype
+ * @subpackage YOURQTYPENAME
+ * @copyright  THEYEAR YOURNAME (YOURCONTACTINFO)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+/**
+ * Set up the plugin.
+ *
+ * @method init
+ */
+export const init = () => {
+  alert('the javascript init code');
+};
